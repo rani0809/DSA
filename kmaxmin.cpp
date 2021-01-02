f1293afb11e5d451bd1fb8b5653dc7a9e4be560e
Rani Dhage <ranidhage007@gmail.com>
2021-01-02 18:46:49 +0530

    Add files via upload

---
+++ kmaxmin.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int k;
+        cin>>k;
+        sort(arr,arr+n);
+        cout<<arr[k-1];
+    }
+    return 0;
+}
