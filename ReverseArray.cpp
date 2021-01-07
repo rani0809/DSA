3e2b315d39fd00a7f84145f79ffa84164683ba84
Rani Dhage <ranidhage007@gmail.com>
2021-01-07 16:56:35 +0530

    Add files via upload

---
+++ ReverseArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+
+        int n=s.length();
+         int start=0,end=n-1;
+
+        while(start<end){
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+        cout<<s<<"\n";
+    }
+    return 0;
+}
